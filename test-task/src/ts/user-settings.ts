1f01eac807e3a953621884f1ecbee7f5fab6c4a2
Evgenia Fedorova <leleoartist@gmail.com>
2022-07-06 02:08:40 +0300

    refactor: change files structure and update readme.md

--- test-task/src/ts/user-settings.ts
+++
@@ -1,8 +0,0 @@
-const accessKey = 'uYroEa_GDkVEWPMRUGRCNotKhvcX7Qm6aj7rqDxlaAM'; // введите свой Access Key
-
-const numberOfImages = 30; // задайте количество изображений на странице (максимум 30)
-const columnCount = 5; // задайте количество колонок
-const widthOFColumn = '150px'; // задайте ширину колонок
-const gap = '7px'; // задайте расстояние между изображениями
-
-export { accessKey, numberOfImages, columnCount, widthOFColumn, gap };
